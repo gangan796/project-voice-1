073946bc864d18774387c657aed8b0eac1c16047
星烬领域 <1684375845@qq.com>
2025-08-01 11:33:55 +0800

    添加一键配置Git代理

---
+++ git_no.bat
@@ -0,0 +1,6 @@
+git config --get http.proxy
+git config --get https.proxy
+git config --unset http.proxy
+git config --unset https.proxy
+git config --get http.proxy
+git config --get https.proxy
---
+++ git_yes.bat
@@ -0,0 +1,4 @@
+git config http.proxy http://127.0.0.1:7890
+git config https.proxy https://127.0.0.1:7890
+git config --get http.proxy
+git config --get https.proxy
